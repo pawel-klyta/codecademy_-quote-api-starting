fa6c6554c913fa54b4ac03792d2766102b311d05
pawel-klyta <pawel.klyta@gmail.com>
2026-07-15 12:47:59 +0200

    All requested GET routes added and tested

--- app.js
+++ app.js
@@ -8,7 +8,29 @@ const { getRandomElement } = require('./utils');
 app.use(cors());
 app.use(express.static('public'));
 
+// GET /api/quotes
 
+app.get("/api/quotes", (req, res, next) => {
+  let result = [];
+  if (req.query.person) {
+    quotes.forEach( quote => {
+      if (quote.person === req.query.person) {
+        result.push(quote);
+      };
+    });
+  } else {
+    result = quotes;
+  };
+
+  const toSend = { quotes: result };
+  res.status(200).send(toSend);
+});
+
+// GET /api/quotes/random
+app.get("/api/quotes/random", (req, res, next) => {
+  const  toSend = { quote: getRandomElement(quotes)}
+  res.status(200).send(toSend);
+});
 
 // export app for use in main.js and for testing
 module.exports = {
--- main.js
+++ main.js
@@ -4,3 +4,7 @@ const {
 
 // designate which PORT the server will listen on
 const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT: ${PORT}.`);
+});
